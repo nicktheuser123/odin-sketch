92a62e6d029b6f048dc01170529a6bcabc2f1e0c
nicktheuser123 <49652976+nicktheuser123@users.noreply.github.com>
2025-08-19 09:44:24 +0530

    added opacity and random color feature

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const container = document.querySelector(".container");
 const button = document.querySelector("button");
 let check;
 let opacity=0.1;
+
+
 button.addEventListener("click",(e)=>{
     let numberH;
     let height;
@@ -14,7 +16,7 @@ button.addEventListener("click",(e)=>{
        numberH = parseInt(height);
         
        check = numberH<0 || numberH > 100 ; 
-       console.log(check);
+    //    console.log(check);
        if(check) alert("Please enter a number between 0 and 100")
     }while(check)
 
@@ -30,7 +32,7 @@ function killChildren(){
     const children = document.querySelectorAll(".row");
     // console.log(children);
     children.forEach((child)=>{
-        console.log(child);  
+        // console.log(child);  
         container.removeChild(child);
         
     })
@@ -46,11 +48,16 @@ function setDimensions(x,y){
             const square = document.createElement("div");
             square.className = "square";
             square.addEventListener("mouseenter",()=>{
-                square.setAttribute("style", "background: blue;");
+                let r = (Math.random() * 255)
+                let g = (Math.random() * 255)
+                let b = (Math.random() * 255)
+
+                // console.log(r,g,b);
+                square.setAttribute("style", `background: rgb(${r},${g},${b});`);
                 if(opacity<1){
                     opacity += 0.1
                 }else{ opacity = 0.1}
-                console.log(opacity)
+                // console.log(opacity)
                 square.style.opacity = opacity
                 // square.style.backgroundColor = "blue";
             })
